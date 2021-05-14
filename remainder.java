17db0b8b3d6f971b0b08ae8c332890c79831107e
siddhendra <sidduchowdary987@gmail.com>
2021-05-14 19:13:56 +0530

    HAS

---
+++ remainder.java
@@ -0,0 +1,12 @@
+class remainder {
+    public static void main(String[] args) {
+        int dividend=300, divisor=30;
+
+        int quotient = dividend / divisor;
+        int remainder = dividend % divisor;
+        System.out.println("the quotient is =" + quotient); 
+        System.out.println("the remainder is =" + remainder);
+    
+    }
+    
+}
---
+++ vowelorcon.java
@@ -0,0 +1,22 @@
+public class vowelorcon {
+    public static void main(String[] args) {
+
+        char ch = 'A';
+
+        switch (ch) {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+                System.out.println(ch + " is vowel");
+                break;
+            default:
+                System.out.println(ch + " is consonant");
+        }
+    }
+
+
+
+    
+}
