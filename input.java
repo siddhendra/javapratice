ac1d6e783cdfeaca23cd8bcb7f6abf4a936b3731
siddhendra <sidduchowdary987@gmail.com>
2021-05-14 16:25:11 +0530

    common

--- input.java
+++ input.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 
 class input {
     public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
+        Scanner scrobj = new Scanner(System.in);
 
         System.out.println("enter your first name :");
-        String firstname = input.next();
+        String firstname = scrobj.next();
 
         System.out.println("enter your last name :");
-        String lastname = input.next();
+        String lastname = scrobj.next();
 
         System.out.println("your full name is "+firstname+""+ lastname);
 
