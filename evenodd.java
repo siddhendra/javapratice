7fc0cd9b4efa34e1ac1bfd14c3cd9eae95ad544d
siddhendra <sidduchowdary987@gmail.com>
2021-05-14 16:03:43 +0530

    message

---
+++ evenodd.java
@@ -0,0 +1,13 @@
+
+class  evenodd
+{
+  public static void main(String args[])
+  {
+    int num = 20;
+
+    if ( num % 2 == 0 )
+        System.out.println("Entered number is even");
+     else
+        System.out.println("Entered number is odd");
+  }
+}
---
+++ swap.java
@@ -0,0 +1,18 @@
+public class swap {
+    
+
+        public static void main(String[] args) {
+         int x = 10;
+         int y = 20;
+        x = x * y;
+        y = x / y;
+        x = x / y;
+         
+         //System.out.println("Values is " + x +" " + y );
+
+         System.out.println("Value of x is :" + x);
+         System.out.println("Value of y is :" + y);
+        }
+       
+    
+}
