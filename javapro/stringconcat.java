130a6c3fae2fbb862b032bc02afbaecccb51fb93
siddhendra <sidduchowdary987@gmail.com>
2021-05-27 21:10:52 +0530

    passed

---
+++ javapro/stringconcat.java
@@ -0,0 +1,9 @@
+public class stringconcat {
+    public static void main(String[] args) {
+        String s1=new String("madala");
+        String s2=new String("siddu");
+        System.out.println(s1+s2);
+        System.out.println(s1.concat(s2));
+    }
+    
+}
